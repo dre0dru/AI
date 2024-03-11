3f36daca99f75260c41ac2c1089c2c7ab798f588
Dre0Dru <dre0druofficial@gmail.com>
2024-03-11 21:52:22 +0300

    chore: cleaned up todos

--- Runtime/BehaviourTree/INode.cs
+++ Runtime/BehaviourTree/INode.cs
@@ -3,9 +3,6 @@ using Dre0Dru.Blackboard;
 
 namespace Dre0Dru.BehaviourTree
 {
-    //TODO IEnumerable?
-    //TODO Atomics?
-    //TODO Blackboard?
     public interface INode : IEnumerable<INode>
     {
         NodeStatus Status { get; }
