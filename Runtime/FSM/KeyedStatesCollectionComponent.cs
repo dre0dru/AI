73ef627916b240e01e09aa1cb2066e0f66ac7fac
Dre0Dru <dre0druofficial@gmail.com>
2024-06-07 09:57:26 +0300

    feat: keyed states collection for fsm

---
+++ Runtime/FSM/KeyedStatesCollectionComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dre0Dru.FSM
+{
+    public class KeyedStatesCollectionComponent<TKey, TState> : MonoBehaviour
+        where TState : IKeyedState<TKey>
+    {
+        private readonly Dictionary<TKey, TState> _states = new();
+
+        public TState this[TKey key] => _states[key];
+
+        public bool TryAddState(TState state)
+        {
+            return _states.TryAdd(state.Key, state);
+        }
+
+        public bool RemoveState(TKey key)
+        {
+            return _states.Remove(key);
+        }
+
+        public bool RemoveState(TState state)
+        {
+            return _states.Remove(state.Key);
+        }
+
+        public bool TryGetState(TKey key, out TState state)
+        {
+            return _states.TryGetValue(key, out state);
+        }
+    }
+}
